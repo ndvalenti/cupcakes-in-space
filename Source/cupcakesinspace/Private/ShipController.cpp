b37785e60577ea2a145cff46b0976d1d89a7a4ba
Nick Valenti <ndvalenti@gmail.com>
2018-03-09 10:30:58 +0100

    refactoring of existing code without meaningful functional change

---
+++ Source/cupcakesinspace/Private/ShipController.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShipController.h"
+
+
+// Add default functionality here for any IShipController functions that are not pure virtual.
